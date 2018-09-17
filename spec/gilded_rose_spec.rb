62a8fb2355e8a8d1404f57b98f7a836581ea8f85
Sean Luckett <whitecrane007@gmail.com>
2018-09-17 16:35:23 -0600

    Improves starting test's quality

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -4,9 +4,11 @@ RSpec.describe GildedRose do
 
   describe '#update_quality' do
     it 'does not change the name' do
-      items = [Item.new('foo', 0, 0)]
-      GildedRose.new(items).update_quality()
-      expect(items[0].name).to eq 'fixme'
+      item = Item.new(name: 'foo', sell_in: 0, quality: 0)
+      items = [item]
+      
+      GildedRose.new(items).update_quality
+      expect(items[0].name).to eq item.name
     end
   end
 
